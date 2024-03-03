35283c412cfe85437fabe6464ee1c591e946224e
Lancelot-SO <fsowah001@gmail.com>
2024-03-03 11:26:41 +0000

    project updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,43 +1,56 @@
-import React from 'react'
-import caller from "../images/caller.png"
+import React, { useEffect } from 'react'
 import "./Contact.css"
+import contact from '../images/contactus.png'
+import logo from "../images/dosh_logo.png"
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Link } from 'react-router-dom';
+
 const Contact = () => {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 2000,
+        });
+        AOS.refresh();
+    }, []);
     return (
-        <div>
-            <div className='contact'>
-                <div className='contact__content'>
-                    <div className='call'>
-                        <img src={caller} alt='caller' loading='lazy' />
-                        <div className="call__modal-body">
-                            <p className='call-head'>Send Us A Message</p>
-                            <form>
-                                <div className="">
-                                    <div className="input-group">
-
-                                        <input type="text" id="fullName" name="fullName" className="form-control" placeholder="Full Name" required />
-                                    </div>
-                                </div>
-
-                                <div className="">
-                                    <div className="input-group">
-
-                                        <input type="email" id="email" name="email" className="form-control" placeholder="Enter Email" required />
-                                    </div>
-                                </div>
-
-                                <textarea id="message" name="message" className='textarea' rows="4" placeholder="Enter your message here"></textarea>
-
-
-                                <div className="">
-                                    <button type="button" className="request">Send Request</button>
-                                </div>
-                            </form>
-
-
+        <div className='cont'>
+            <div className='contact__head'>
+                <img data-aos="fade-down" src={contact} alt='about' loading='lazy' />
+                <div className='contact__text'>
+                    <p>
+                        Contact US
+                    </p>
+                </div>
+            </div>
+            <div className='contact__main'>
+                <form action='#'>
+                    <p>Get in touch with us</p>
+                    <div>
+                        <div className='input__form'>
+                            <input type='text' name='name' placeholder='Your name*' required />
+                            <input type='email' name='email' placeholder='Email*' required />
 
                         </div>
+                        <div className='input__form'>
+                            <input type='tel' name='phone' placeholder='Phone Number*' required />
+                            <input type='text' name='city' placeholder='City*' required />
+
+                        </div>
+                        <textarea name="message" rows="4" placeholder='Your Message'></textarea>
+                        <div className='form__btn'>
+                            <button className='submit__btn' type='submit'>Submit Message</button>
+                        </div>
+
                     </div>
-                </div>
+                    <div className='contact__logo'>
+                        <Link to='/' className="cont__logo">
+                            <img src={logo} alt='navbar logo' />
+                        </Link>
+                    </div>
+                </form>
             </div>
         </div>
     )
