b68aa51abf24a54aa7b8a0f725c04f7dd7f652ac
Lancelot-SO <fsowah001@gmail.com>
2024-03-06 19:03:13 +0000

    project updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import logo from "../images/dosh_logo.png";
-import { MdLocationOn } from "react-icons/md";
-import { FaMobileAlt, FaTwitter, FaFacebook, FaYoutube, FaInstagram, FaLinkedin, FaWhatsapp, FaTiktok } from "react-icons/fa";
-import { IoIosMail } from "react-icons/io";
+import { FaTwitter, FaFacebook, FaYoutube, FaInstagram, FaLinkedin, FaTiktok } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 //
 
@@ -12,64 +10,55 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div>
-            <div className='footer__gold'></div>
             <footer>
                 <div className='container footer__container'>
-                    <div className='footer__logo'>
-                        <Link to='/' className='nav__logo'>
-                            <img src={logo} alt='navbar logo' />
-                        </Link>
-                        <div className='footer__contacts'>
-                            <div className='footer__icon-texts'>
-                                <div className='footer__icon-text1'>
-                                    <MdLocationOn size={30} />
-                                    <h4>8 Sir Arku Korsah, Rd Airport,
-                                        Residential Area  Accra, Ghana</h4>
-                                </div>
-                                <div>
-                                    <div className='footer__icon-text2'>
-                                        <FaMobileAlt />
-                                        <h4>0800-DOSH-ME</h4>
-                                    </div>
-                                    <div className='footer__icon-text3'>
-                                        <IoIosMail />
-                                        <h4>info@0800dosh.me</h4>
-                                    </div>
-                                    <div className='footer__icon-text3'>
-                                        <FaWhatsapp />
-                                        <h4>+1 (203) 293-9850</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='social__icons'>
-                                <Link to='https://x.com/dosh_revolution?s=21&t=-BrXbfatLtONPkJKS4q8HQ' target="_blank" rel="noopener noreferrer">
-                                    <FaTwitter size={24} />
-                                </Link>
-                                <Link to='https://www.facebook.com/DOSH.Revolution?mibextid=LQQJ4d' target="_blank" rel="noopener noreferrer">
-                                    <FaFacebook size={24} />
-                                </Link>
-                                <Link to='https://www.linkedin.com/company/dosh-revolution/' target="_blank" rel="noopener noreferrer">
-                                    <FaLinkedin size={24} />
-                                </Link>
-                                <Link to='https://www.instagram.com/dosh_revolution?igsh=MXQ3Z2d6aTMxMHA3ZA%3D%3D&utm_source=qr' target="_blank" rel="noopener noreferrer">
-                                    <FaInstagram size={24} />
-                                </Link>
-                                <Link to='https://youtube.com/@DOSHRevolution?si=H6MOS8Wj6Se8eegI' target="_blank" rel="noopener noreferrer">
-                                    <FaYoutube size={24} />
-                                </Link>
-                                <Link to='https://www.tiktok.com/@dosh.revolution?_t=8kJif6YpYMX&_r=1' target="_blank" rel="noopener noreferrer">
-                                    <FaTiktok size={24} />
-                                </Link>
-
-
-
-
+                    <div className='contact__logo'>
+                        <div className='footer__logo'>
+                            <img src={logo} alt='dosh-logo' />
+                        </div>
+                        <div className='footer__text'>
+                            <p>8 Sir Arku Korsah <br /> Rd. Airport <br />Residential Area <br />Accra, Ghana</p>
+                        </div>
+                        <div className='contacts'>
+                            <h3>    CONTACTS</h3>
+                            <div className='location'>
+                                <p>Phone: 0800-DOSH-ME</p>
+                                <p>Fax: 0800-DOSH-ME</p>
+                                <p>Online Support</p>
+                                <p>Request Service</p>
                             </div>
                         </div>
-
                     </div>
+                    <div className='socials'>
+                        <Link to='https://x.com/dosh_revolution?s=21&t=-BrXbfatLtONPkJKS4q8HQ' target="_blank" rel="noopener noreferrer">
+                            <FaTwitter size={24} />
+                        </Link>
+                        <Link to='https://www.facebook.com/DOSH.Revolution?mibextid=LQQJ4d' target="_blank" rel="noopener noreferrer">
+                            <FaFacebook size={24} />
+                        </Link>
+                        <Link to='https://www.linkedin.com/company/dosh-revolution/' target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin size={24} />
+                        </Link>
+                        <Link to='https://www.instagram.com/dosh_revolution?igsh=MXQ3Z2d6aTMxMHA3ZA%3D%3D&utm_source=qr' target="_blank" rel="noopener noreferrer">
+                            <FaInstagram size={24} />
+                        </Link>
+                        <Link to='https://youtube.com/@DOSHRevolution?si=H6MOS8Wj6Se8eegI' target="_blank" rel="noopener noreferrer">
+                            <FaYoutube size={24} />
+                        </Link>
+                        <Link to='https://www.tiktok.com/@dosh.revolution?_t=8kJif6YpYMX&_r=1' target="_blank" rel="noopener noreferrer">
+                            <FaTiktok size={24} />
+                        </Link>
+                    </div>
+
+
                 </div>
+
             </footer>
+            <div className='footer__gold'></div>
+            <div className='container copyright'>
+                <p>Powered by OPIN Technologies ®</p>
+            </div>
+
         </div>
     )
 }
