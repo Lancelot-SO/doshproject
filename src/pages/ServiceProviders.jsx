a368e26591bb5fe8c22bf752ce7a22f47cceb526
Lancelot-SO <fsowah001@gmail.com>
2024-05-07 11:36:02 +0000

    project updated

--- src/pages/ServiceProviders.jsx
+++ src/pages/ServiceProviders.jsx
@@ -5,7 +5,7 @@ import { FaFilter } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 // import anime from '../images/Enterprise1.png'
 import data from "../data"
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+// import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import FilterModal from '../components/FilterModal';
 import CallModal from '../components/CallModal';
 // import { Link } from 'react-router-dom';
@@ -96,46 +96,6 @@ const ServiceProviders = () => {
                 </div>
             </section>
 
-
-
-            <section className='serviceslider'>
-                <div className='section-center'>
-                    <h1 className='testimonial'>Testimonials</h1>
-
-                    {people.map((item, indexPeople) => {
-                        const { id } = item;
-                        let position = "nextSlide";
-                        if (indexPeople === index) {
-                            position = "activeSlide";
-                        }
-                        if (indexPeople === index - 1 || (index === 0 && indexPeople === people.length - 1)) {
-                            position = "lastSlide";
-                        }
-                        return (
-                            <article className={position} key={id}>
-                                <div className='divide'>
-                                    <img src={item.pic} alt='person' className="service-img" />
-                                    <div className='service-name-quote'>
-                                        <span className='service-name'>{item.name}</span>
-                                        <p className='service-quote'>{item.testimony}</p>
-                                    </div>
-                                </div>
-                            </article>
-                        );
-                    })}
-                    <button className='previous' onClick={() => setIndex(index - 1)}>
-                        <IoIosArrowBack size={30} />
-                    </button>
-                    <button className='nextitem' onClick={() => setIndex(index + 1)}>
-                        <IoIosArrowForward size={30} />
-                    </button>
-                    <div className="slider-indicators">
-                        <span className='slider-total'>{`${(index + 1).toString().padStart(2, '0')}/${people.length.toString().padStart(2, '0')}`}</span>
-                    </div>
-                </div>
-
-
-            </section>
 
         </div>
     )
