05aed02b3f1392b6f0261bc155cc9fde5824fce3
unknown <fsowah001@gmail.com>
2024-09-25 11:21:32 +0000

    update

--- src/doshdata.js
+++ src/doshdata.js
@@ -73,6 +73,8 @@ export const packagelist = [
     },
 ]
 
+// the link was to /insurance and i changed it to https://dsp.onenet.xyz:50443/#/?page=dosh-insurance remember to change when portal is ready
+
 export const insuranceDetails = [
     {
         id: "1",
@@ -81,7 +83,7 @@ export const insuranceDetails = [
         number: "365",
         desc: "Description",
         details: `Dosh is rewriting the story on health insurance with the DOSH365 package. This package is highly affordable and offers a new health insurance experience designed with the individual in mind. An amount of GHS 1 per day, or a total of GHS 365 per year, provides a membership cover of GHS 9,000 annually. Out of the GHS 9,000, an amount of GHS 7,500 is allocated to in-patient services, and the remaining GHS 1,500 is for out-patient services. DOSH 365 is specifically designed to shoulder the unforeseen medical situations`,
-        link: "https://dsp.onenet.xyz:50443/#/",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-insurance",
         category: "DOSH 365"
     },
     {
@@ -91,7 +93,7 @@ export const insuranceDetails = [
         number: "750",
         desc: "Description",
         details: `DOSH 750 comes with add-ons and quintessential incentives targeted at individuals, families, and their dependents, with a slightly higher premium. It is gratifying to know that with DOSH, your family and dependents’ health care needs are covered with a cover-up of up to GHS 18,000 for only a GHS 750 annual premium. This package is perfect for new employees or young families looking for affordable health insurance. An annual premium of GHS 750 comes with health insurance coverage of up to GHS 15,000 for in-patient and GHS 3,000 for out-patient services, respectively.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-insurance",
         category: "DOSH 750"
     },
     {
@@ -101,7 +103,7 @@ export const insuranceDetails = [
         number: "1000",
         desc: "Description",
         details: `The package is distinctively designed to offer members absolute serenity with regard to their healthcare needs, offering an extended cover of up to a whopping GHS 30,000 for an annual premium of only GHS 1,000. DOSH1000 offers an in-patient limit of GHS 25,000 and an out-patient limit of GHS 5,000, offering members value for their investments.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-insurance",
         category: "DOSH 1000"
     },
     {
@@ -112,7 +114,7 @@ export const insuranceDetails = [
         desc: "Description",
         details: `This package is ideal for new employees or young families who want affordable health insurance.
 For people and businesses looking to manage their finances wisely and prepare for tough times, the DOSH 2500 health insurance plan is highly recommended. It offers annual coverage of up to GHS 60,000. DOSH 2500 offers GHS 50,000 in-patient and GHS 10,000 out-patient services for an annual premium of GHS 2,500.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-insurance",
         category: "DOSH 2500"
     },
     {
@@ -123,7 +125,7 @@ For people and businesses looking to manage their finances wisely and prepare fo
         desc: "Description",
         details: `This premium offer is tailored to meet the health insurance needs of our high-value clients. It provides excellent healthcare services with annual coverage of up to GHS 95,000, including GHS 75,000 for in-patient care and GHS 20,000 for out-patient care. 
 This package is recommended for individuals or businesses with amplified healthcare needs.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-insurance",
         category: "DOSH 5000"
     },
     {
@@ -134,7 +136,7 @@ This package is recommended for individuals or businesses with amplified healthc
         desc: "Description",
         details: `The DOS 10,000 plan offers up to GHS 190,000 in health coverage, perfect for those seeking comprehensive healthcare and peace of mind. With this plan, you get GHS 150,000 for in-patient care and GHS 40,000 for out-patient care, all for an annual premium of GHS 10,000. 
 The services offered by this plan are accessible at all significant healthcare providers around the country.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-insurance",
         category: "DOSH 10000"
     },
 ]
@@ -168,7 +170,7 @@ export const financeDetails = [
         title: "Personal",
         desc: "Description",
         details: `Whether at home or at work, banking shouldn’t be a problem with Dosh. Our uniquely designed and tailor-made personal account offers 1 user with 5 different types of accounts, making it easy for our clients to always stay on top of their personal finances. We have the option of additional accounts at configurable fee per each additional account.`,
-        link: "https://dsp.onenet.xyz:50443/#/",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-financial",
         category: "Personal"
     },
     {
@@ -177,7 +179,7 @@ export const financeDetails = [
         title: "Family",
         desc: "Description",
         details: `Family financial management involves effectively meeting your family's everyday expenses, handling unexpected bills and saving for the future. At DOSH, our Family account puts you in control of your money, helping you avoid stress, whilst making you feel more secured. In addition to the 5 different accounts available with the Personal Account package, the Family Account offers 5 additional users with current sub accounts.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-financial",
         category: "Family"
     },
     {
@@ -186,7 +188,7 @@ export const financeDetails = [
         title: "SOHO",
         desc: "Description",
         details: `Your SOHO Account comes with multiple perks, positioning your business for growth. With the SOHO account, you have access to loan and credit facilities as well as protecting your personal assets by separating them from your business transactions. This package comes with our default 5 accounts with 5 additional current sub-accounts and 2 other departmental or cost centers. The departments or cost centers are configured with different levels of access for you and your employees within the SOHO set up. The access levels help in assigning and controlling staff who for instance, can create new users, add bank accounts, perform financial transactions, view and move money from any account, audit accounts and set limits on sub-accounts among others.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-financial",
         category: "SOHO"
     },
     {
@@ -195,7 +197,7 @@ export const financeDetails = [
         title: "SMB",
         desc: "Description",
         details: `This account has advanced benefits and features compared to the SOHO Account. It comes with 5 additional users and a total of 5 departments or cost centers with the convenient option of adding more users as the business grows. In effect, the SMB Account offers subscribers 10 sub accounts and 5 departments or call centers with different levels of access and control.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-financial",
         category: "SMB"
     },
     {
@@ -204,7 +206,7 @@ export const financeDetails = [
         number: "Enterpirse",
         desc: "Description",
         details: `The Enterprise Account is designed to provide financial solutions to large corporations and comes with same features as the SMB Account with added offerings: 20 additional users 5 additional departments or call centers. The enterprise account therefore offers customers with the convenience of 30 users and 10 departments or call centers; in addition to all the default benefits embedded in our SOHO Account.`,
-        link: "/insurance",
+        link: "https://dsp.onenet.xyz:50443/#/?page=dosh-financial",
         category: "Enterpirse"
     },
 ]
